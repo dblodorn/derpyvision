e9fb239b4f877b8d5af7254e0b981c0ef4d84e4a
Dain Blodorn Kim <dainblodornkim@dains-imac.lan>
2019-12-05 17:08:49 -0800

    chunk out threejs stuff

---
+++ src/views/home/ThreeBg.vue
@@ -0,0 +1,21 @@
+<template>
+  <ThreeDiv/>
+</template>
+
+<script>
+import styled from 'vue-styled-components'
+import threeShaderBg from '@/three/threeShaderBg'
+
+const ThreeDiv = styled.div`
+  display: none;
+`
+
+export default {
+  components: {
+    ThreeDiv
+  },
+  mounted: () => {
+    threeShaderBg()
+  },
+}
+</script>
